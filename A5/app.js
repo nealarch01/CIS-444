cfc5a75cf01eb2ae3ac73de7d137d3d96fac4260
nealarch01 <archivalneal@gmail.com>
2022-03-10 15:29:48 -0800

    added assignment 5

---
+++ A5/app.js
@@ -0,0 +1,25 @@
+var state = 'hidden'; // state can equal to hidden or shown
+function hideImage(elem) {
+    elem.style.display = 'none';
+}
+
+function showImage(elem) {
+    elem.style.display = 'inline-block';
+}
+
+document.getElementById('main-body').addEventListener('click', (event) => {
+    var imgContainer = document.getElementById('image-container');
+    var imgHeight = document.getElementById('my-image').getAttribute('height');
+    var imgWidth = document.getElementById('my-image').getAttribute('width');
+    // imgHeight.match(/\d+/);
+    // console.log(imgHeight);
+    if (state === 'hidden') {
+        imgContainer.style.top = `${event.y - parseInt(imgHeight / 2)}px`;
+        imgContainer.style.left = `${event.x - parseInt(imgWidth / 2)}px`
+        showImage(imgContainer);
+        state = 'shown';
+    } else { // state === 'shown'
+        hideImage(imgContainer);
+        state = 'hidden';
+    }
+});
