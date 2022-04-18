9ec8cfae2a655f5ef2bcf6aa7fc479ffffd31a1a
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-18 16:47:08 -0700

    Used loop to print table row header cells (field names)

--- A6/nba-team.php
+++ A6/nba-team.php
@@ -10,45 +10,44 @@
         <h3 class="main-header"> 
         <?php
             $winsInput = $_POST["wins"];
-            echo "Teams with greater than or equal to: {$winsInput} wins";
+            echo "Teams with {$winsInput} or more wins";
             $mySQLDB = mysqli_connect('localhost', 'root', '12345');
             if (!$mySQLDB) {
                 echo "<br>There was an error doing a query";
                 mysqli_close($mySQLDB);
                 exit;
             }
-            $selectedDB = mysqli_select_db($mySQLDB, "Assignment6");
-            if (!$selectedDB) {
-                echo "<br>There was an error opening the database";
-                mysqli_close($mySQLDB);
+
+            // using try catch exception block since mysqli_select_db throws an exception and cannot be caught by an if statement
+            try {
+                $selectedDB = mysqli_select_db($mySQLDB, "Assignment6");
+            } catch (mysqli_sql_exception) {
+                echo "<br>There was an error selecting the database";
                 exit;
             }
+
             $queryString = "SELECT * FROM Teams WHERE overallWins >= {$winsInput}";
             $queryResult = mysqli_query($mySQLDB, $queryString);
-            echo "<table>";
-            echo "<tr>" . 
-                    "<th>Team ID</th>" .
-                    "<th>Team Name</th>" .
-                    "<th>Start Year</th>" .
-                    "<th>Owner</th>" .
-                    "<th>General Manager</th>" .
-                    "<th>Coach</th>" .
-                    "<th>MVP</th>" .
-                    "<th>Arena Address</th>" .
-                    "<th>City</th>" .
-                    "<th>State</th>" .
-                    "<th>ZIP</th>" .
-                    "<th>Phone #</th>" .
-                    "<th>Div Wins</th>" .
-                    "<th>Div Loses</th>" .
-                    "<th>Conf Wins</th>" .
-                    "<th>Conf Loses</th>" .
-                    "<th>Overall Wins</th>" .
-                    "<th>Overall Loses</th>" .
-                "</tr>";
+            $numberOfResults = mysqli_num_rows($queryResult);
+
+            // do not print the table if there are not results found
+            if ($numberOfResults == 0) {
+                echo "<br>There are no teams with {$winsInput} wins or more";
+                exit;
+            }
+
+            // start of table
+            echo "<table> <tr>";
+            // printing all the columns
+            while ($singleColumn = mysqli_fetch_field($queryResult)) {
+                echo "<th>{$singleColumn->name}</th>";
+            }
+            echo "</tr>";
+
+            // printing all row and cell data
             while ($singleRowData = mysqli_fetch_assoc($queryResult)) {
                 echo "<tr>";
-                    foreach($singleRowData as $value) {
+                    foreach ($singleRowData as $value) {
                         echo "<td> {$value} </td>";
                     }
                 echo "</tr>";
